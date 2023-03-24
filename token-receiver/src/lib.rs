4dad09f2a31721c218e135187cfafef86c327f11
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-03-24 19:31:37 +0100

    Token receiver & Workshop separate init

---
+++ token-receiver/src/lib.rs
@@ -0,0 +1,102 @@
+use near_contract_standards::non_fungible_token::core::NonFungibleTokenReceiver;
+use near_contract_standards::non_fungible_token::TokenId;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
+use near_sdk::{
+    env, log, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, PromiseOrValue,
+};
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct TokenReceiver {
+    pub nft_account_ids: Vec<AccountId>,
+}
+
+#[near_bindgen]
+impl TokenReceiver {
+    #[init]
+    pub fn new(nft_account_ids: Vec<AccountId>) -> Self {
+        Self {
+            nft_account_ids: nft_account_ids.into(), // it worked, probably, can be changed to nft_account_ids,
+        }
+    }
+    fn insert_bicycle_component_to_crafting_table(
+        &mut self,
+        previous_owner_id: AccountId,
+        token_id: TokenId,
+        msg: String,
+    ) {
+        let crafting_table = self
+            .crafting_tables_per_owner
+            .get(&previous_owner_id)
+            .unwrap_or_else(|| CraftingTable {
+                wheels: None,
+                handlebar: None,
+                frame: None,
+                saddle: None,
+                transmission: None,
+                brakes: None,
+            });
+        let new_crafting_table = CraftingTable {
+            wheels: if msg.as_str() == "wheels" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.wheels
+            },
+            handlebar: if msg.as_str() == "handlebar" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.handlebar
+            },
+            frame: if msg.as_str() == "frame" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.frame
+            },
+            saddle: if msg.as_str() == "saddle" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.saddle
+            },
+            transmission: if msg.as_str() == "transmission" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.transmission
+            },
+            brakes: if msg.as_str() == "brakes" {
+                Some(token_id.clone())
+            } else {
+                crafting_table.brakes
+            },
+        };
+        self.crafting_tables_per_owner
+            .insert(&previous_owner_id, &new_crafting_table);
+    }
+}
+
+#[near_bindgen]
+impl NonFungibleTokenReceiver for TokenReceiver {
+    fn nft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        previous_owner_id: AccountId,
+        token_id: TokenId,
+        msg: String,
+    ) -> PromiseOrValue<bool> {
+        // Verifying that we were called by non-fungible token contract that we expect.
+        assert!(self
+            .nft_account_ids
+            .contains(&env::predecessor_account_id()));
+        log!(
+            "in nft_on_transfer; sender_id={}, previous_owner_id={}, token_id={}, msg={}",
+            &sender_id,
+            &previous_owner_id,
+            &token_id,
+            msg
+        );
+
+        self.insert_bicycle_component_to_crafting_table(previous_owner_id, token_id, msg);
+
+        PromiseOrValue::Value(false)
+    }
+}
--- workshop/src/lib.rs
+++ workshop/src/lib.rs
@@ -1,21 +1,29 @@
-use near_contract_standards::non_fungible_token::core::NonFungibleTokenReceiver;
 use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
-use near_sdk::{
-    env, log, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, PromiseOrValue,
-};
+use near_sdk::{near_bindgen, AccountId, BorshStorageKey, PanicOnDefault};
 
 #[derive(BorshSerialize, BorshStorageKey)]
 enum StorageKey {
-    CraftingTable,
+    CraftingTables,
+    ComponentWhitelist,
+}
+
+#[derive(BorshDeserialize, BorshSerialize)]
+pub enum BicycleComponent {
+    Wheels,
+    Handlebar,
+    Frame,
+    Saddle,
+    Transmission,
+    Brakes,
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Workshop {
-    pub nft_account_ids: Vec<AccountId>,
     pub crafting_tables_per_owner: LookupMap<AccountId, CraftingTable>,
+    pub component_nft_whitelist: LookupMap<AccountId, BicycleComponent>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -31,89 +39,10 @@ pub struct CraftingTable {
 #[near_bindgen]
 impl Workshop {
     #[init]
-    pub fn new(nft_account_ids: Vec<AccountId>) -> Self {
+    pub fn new() -> Self {
         Self {
-            nft_account_ids: nft_account_ids.into(), // it worked, probably, can be changed to nft_account_ids,
-            crafting_tables_per_owner: LookupMap::new(StorageKey::CraftingTable),
+            crafting_tables_per_owner: LookupMap::new(StorageKey::CraftingTables),
+            component_nft_whitelist: LookupMap::new(StorageKey::ComponentWhitelist),
         }
-    }
-    fn insert_bicycle_component_to_crafting_table(
-        &mut self,
-        previous_owner_id: AccountId,
-        token_id: TokenId,
-        msg: String,
-    ) {
-        let crafting_table = self
-            .crafting_tables_per_owner
-            .get(&previous_owner_id)
-            .unwrap_or_else(|| CraftingTable {
-                wheels: None,
-                handlebar: None,
-                frame: None,
-                saddle: None,
-                transmission: None,
-                brakes: None,
-            });
-        let new_crafting_table = CraftingTable {
-            wheels: if msg.as_str() == "wheels" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.wheels
-            },
-            handlebar: if msg.as_str() == "handlebar" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.handlebar
-            },
-            frame: if msg.as_str() == "frame" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.frame
-            },
-            saddle: if msg.as_str() == "saddle" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.saddle
-            },
-            transmission: if msg.as_str() == "transmission" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.transmission
-            },
-            brakes: if msg.as_str() == "brakes" {
-                Some(token_id.clone())
-            } else {
-                crafting_table.brakes
-            },
-        };
-        self.crafting_tables_per_owner
-            .insert(&previous_owner_id, &new_crafting_table);
-    }
-}
-
-#[near_bindgen]
-impl NonFungibleTokenReceiver for Workshop {
-    fn nft_on_transfer(
-        &mut self,
-        sender_id: AccountId,
-        previous_owner_id: AccountId,
-        token_id: TokenId,
-        msg: String,
-    ) -> PromiseOrValue<bool> {
-        // Verifying that we were called by non-fungible token contract that we expect.
-        assert!(self
-            .nft_account_ids
-            .contains(&env::predecessor_account_id()));
-        log!(
-            "in nft_on_transfer; sender_id={}, previous_owner_id={}, token_id={}, msg={}",
-            &sender_id,
-            &previous_owner_id,
-            &token_id,
-            msg
-        );
-
-        self.insert_bicycle_component_to_crafting_table(previous_owner_id, token_id, msg);
-
-        PromiseOrValue::Value(false)
     }
 }
